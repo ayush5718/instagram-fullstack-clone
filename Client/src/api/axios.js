c7fd675368a9657a8cb8dce8d2ff183dc5418e7f
Aayush Kumar <ayushbhardwaj9504@gmail.com>
2024-11-02 18:46:17 +0530

    backend url added

--- Client/src/api/axios.js
+++ Client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://instagram-clone-backend-rosy.vercel.app/api/v1",
+  baseURL: "https://instagram-fullstack-clone-backend.vercel.app/api/v1",
   headers: {
     "Content-type": "application/json",
   },
